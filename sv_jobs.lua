55450c47870b20fcfe7eff2c8fa855bcdb6617b1
Randolio <103154494+Randolio@users.noreply.github.com>
2023-04-22 01:05:14 +0100

    Tidy up code.

--- sv_jobs.lua
+++ sv_jobs.lua
@@ -6,13 +6,17 @@ lib.callback.register('randol_multijob:server:myJobs', function(source)
     local storeJobs = {}
     local result = MySQL.query.await('SELECT * FROM save_jobs WHERE cid = ?', {Player.PlayerData.citizenid})
     for k, v in pairs(result) do
-        storeJobs[#storeJobs + 1] = {
-            job = v.job,
-            salary = QBCore.Shared.Jobs[v.job].grades[tostring(v.grade)].payment,
-            jobLabel = QBCore.Shared.Jobs[v.job].label,
-            gradeLabel = QBCore.Shared.Jobs[v.job].grades[tostring(v.grade)].name,
-            grade = v.grade,
-        }
+        local job = QBCore.Shared.Jobs[v.job]
+        if job then
+            local grade = job.grades[tostring(v.grade)]
+            storeJobs[#storeJobs + 1] = {
+                job = v.job,
+                salary = grade.payment,
+                jobLabel = job.label,
+                gradeLabel = grade.name,
+                grade = v.grade,
+            }
+        end
     end
     return storeJobs
 end)
@@ -20,27 +24,28 @@ end)
 RegisterNetEvent('randol_multijob:server:changeJob', function(job, grade)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local JobInfo = QBCore.Shared.Jobs[job]
     if Player.PlayerData.job.name == job then QBCore.Functions.Notify(src, 'Your current job is already set to this.', 'error') return end
-    TriggerClientEvent("QBCore:Client:SetDuty", src, false)
+
+    local jobInfo = QBCore.Shared.Jobs[job]
+    if not jobInfo then QBCore.Functions.Notify(src, 'Invalid job.', 'error') return end
+
     Player.Functions.SetJob(job, grade)
     Player.Functions.SetJobDuty(false)
-    QBCore.Functions.Notify(src, 'Your job is now: '..JobInfo.label .. '.')
+    TriggerClientEvent('QBCore:Client:SetDuty', src, false)
+    QBCore.Functions.Notify(src, 'Your job is now: ' .. jobInfo.label)
 end)
 
 RegisterNetEvent('randol_multijob:server:newJob', function(newJob)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local hasJob = false
-    local result = MySQL.query.await('SELECT * FROM save_jobs WHERE cid = ?', {Player.PlayerData.citizenid})
-    for k, v in pairs(result) do
-        if newJob.name == v.job then
-            MySQL.query.await('UPDATE save_jobs SET grade = ? WHERE job = ? and cid = ?', {newJob.grade.level, newJob.name, Player.PlayerData.citizenid})
-            hasJob = true
-        end
+    local result = MySQL.query.await('SELECT * FROM save_jobs WHERE cid = ? AND job = ?', {Player.PlayerData.citizenid, newJob.name})
+    if result[1] then
+        MySQL.query.await('UPDATE save_jobs SET grade = ? WHERE job = ? and cid = ?', {newJob.grade.level, newJob.name, Player.PlayerData.citizenid})
+        hasJob = true
     end
     if not hasJob then 
-        MySQL.insert.await('INSERT INTO save_jobs (cid, job, grade) VALUE (?, ?, ?)', {Player.PlayerData.citizenid, newJob.name, newJob.grade.level})
+	MySQL.insert.await('INSERT INTO save_jobs (cid, job, grade) VALUE (?, ?, ?)', {Player.PlayerData.citizenid, newJob.name, newJob.grade.level})
     end
 end)
 
@@ -48,7 +53,7 @@ RegisterNetEvent('randol_multijob:server:deleteJob', function(job)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if job == 'unemployed' then QBCore.Functions.Notify(src, 'This job can\'t be removed.', 'error') return end
-    MySQL.query.await('DELETE FROM save_jobs WHERE cid = ? and job = ?', {Player.PlayerData.citizenid, job})
+    MySQL.query.await('DELETE FROM `save_jobs` WHERE `cid` = ? and `job` = ?', {Player.PlayerData.citizenid, job})
     QBCore.Functions.Notify(src, 'You deleted '..QBCore.Shared.Jobs[job].label..' job from your menu.')
     Player.Functions.SetJob('unemployed', 0)
 end)
