bcf9f7b2c27e34dab277d6e3e4a3fb1d35831e9c
Randolio <103154494+Randolio@users.noreply.github.com>
2023-06-02 20:04:40 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.MaxJobs = 5
+
+Config.JobIcons = {
+	['police'] = 'fa-solid fa-shield',
+	['ambulance'] = 'fa-solid fa-user-doctor',
+	['electrician'] = 'fa-solid fa-bolt',
+	['pdtow'] = 'fa-solid fa-truck-pickup',
+	['judge'] = 'fa-solid fa-gavel',
+	['realestate'] = 'fa-solid fa-house',
+	['cardealer'] = 'fa-solid fa-car',
+	['garbage'] = 'fa-solid fa-trash',
+	['casino'] = 'fa-solid fa-dice',
+	['mechanic'] = 'fa-solid fa-wrench',
+	['tow'] = 'fa-solid fa-truck-tow',
+	['taxi'] = 'fa-solid fa-taxi',
+	['vineyard'] = 'fa-solid fa-wine-bottle',
+	['hotdog'] = 'fa-solid fa-hotdog',
+}
