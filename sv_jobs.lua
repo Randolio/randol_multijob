b33a30d4202fe4d0a5f87f1d76b99c4f7b320cbe
Randolio <103154494+Randolio@users.noreply.github.com>
2023-12-18 20:16:26 +0000

    Update sv_jobs.lua

--- sv_jobs.lua
+++ sv_jobs.lua
@@ -63,7 +63,7 @@ end)
 RegisterNetEvent('randol_multijob:server:deleteJob', function(job)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    MySQL.query.await('DELETE FROM `save_jobs` WHERE `cid` = ? and `job` = ?', {Player.PlayerData.citizenid, job})
+    MySQL.query.await('DELETE FROM save_jobs WHERE cid = ? and job = ?', {Player.PlayerData.citizenid, job})
     QBCore.Functions.Notify(src, 'You deleted '..QBCore.Shared.Jobs[job].label..' job from your menu.')
     Player.Functions.SetJob('unemployed', 0)
 end)
